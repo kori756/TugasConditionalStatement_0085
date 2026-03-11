2c0f8398823d897bbc906ca9653b82e701e1f401
kori756 <khorirachmatr@gmail.com>
2026-03-11 08:56:23 +0700

    memperbaiki struktur bmi

--- MenghitungBMI.cpp
+++ MenghitungBMI.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 using namespace std;
 
-float NilBB, NilTB, perhitungan_BMI;
-    string bmi;
-
-cout << "Masukan Berat Badan(KG) : " ;   
-cin >> NilBB ;
-cout << "Masukan Tinggi badan(METER) : " ;
-cin >> NilTB ;
-
-float perhitungan_bmi(float a,float b){
+float perhitungan_BMI(float a,float b){
     return a/(b*b);
 }
  string bmi (float r){
-    if(r<18.5)
+    if(r < 18.5) {
         return "berat badan kurang";
-    else{
-        if(r<=25.00){
-            return "berat badan normal";
-        }
-        else{
-            if(r<=27.00){
-                return "kelebihan berat badan";
-            }
-            else{
-                return "obsitas";
-            }
-        }
+    } else if(r <= 25.00) {
+        return "berat badan normal";
+    } else if(r <= 27.00) {
+        return "kelebihan berat badan";
+    } else {
+        return "obesitas"; 
     }
- }
+}
+
+ int main(){
+    float NilBB, NilTB;
+
+    cout << "Masukan Berat Badan(KG) : " ;   
+    cin >> NilBB ;
+    cout << "Masukan Tinggi badan(METER) : " ;
+    cin >> NilTB ;
+    
+    cout << "BMI Anda = " <<perhitungan_BMI<<",dengan status = " << perhitungan_BMI<<endl;
+ 
+}
+
+
+
+
 
 
 
