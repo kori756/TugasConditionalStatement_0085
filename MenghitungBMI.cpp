aafc9cce0e83ba7112a1c13b29ee8f32017e4819
kori756 <khorirachmatr@gmail.com>
2026-03-11 08:26:09 +0700

    membuat struktur bmi

--- MenghitungBMI.cpp
+++ MenghitungBMI.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+float NilBB, NilTB, perhitungan_BMI;
+    string bmi;
+
+cout << "Masukan Berat Badan(KG) : " ;   
+cin >> NilBB ;
+cout << "Masukan Tinggi badan(METER) : " ;
+cin >> NilTB ;
+
+
+
+ 
